b183f61f53d54b9df7c762b9183acf4d9288b158
OOleksII <kozzzak@gmail.com>
2017-08-05 11:10:51 +0300

    implemented IQueryDispatcher

--- CQRS/CQRSDemoApp/Query/IQueryDispatcher.cs
+++ CQRS/CQRSDemoApp/Query/IQueryDispatcher.cs
@@ -1,7 +1,32 @@
-﻿namespace CQRSDemoApp.Query
+﻿using System;
+
+namespace CQRSDemoApp.Query
 {
     public interface IQueryDispatcher
     {
         TResult Execute<TQuery, TResult>(TQuery query) where TQuery : IQuery<TResult>;
+    }
+
+    public class QueryDispatcher : IQueryDispatcher
+    {
+        private readonly IDependencyResolver _resolver;
+
+        public QueryDispatcher(IDependencyResolver resolver)
+        {
+            _resolver = resolver;
+        }
+
+        public TResult Execute<TQuery, TResult>(TQuery query) where TQuery : IQuery<TResult>
+        {
+            if (query == null)
+                throw new ArgumentNullException("query");
+
+            var handler = _resolver.Resolve<IQueryHandler<TQuery, TResult>>();
+
+            if (handler == null)
+                throw new NullReferenceException("not found");
+
+            return handler.Execute(query);
+        }
     }
 }
